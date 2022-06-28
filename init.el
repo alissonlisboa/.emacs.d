e535034bb648fc309efcb23b0fb542ad41c8e580
Alisson Lisboa <alisson_lisboa@hotmail.com>
2022-06-28 19:46:53 -0300

    initial commit

---
+++ init.el
@@ -0,0 +1,12 @@
+;; Interface
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
+(setq inhibit-startup-screen t)
+
+;; Funcionality
+(electric-pair-mode 1)
+
